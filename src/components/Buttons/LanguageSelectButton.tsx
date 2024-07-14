aed116a47367a6e318fabbd73e4c3e8047186845
Skiftstar <yukamitft@gmail.com>
2024-07-14 12:27:19 +0200

    Professional Language

--- src/components/Buttons/LanguageSelectButton.tsx
+++ src/components/Buttons/LanguageSelectButton.tsx
@@ -1,24 +1,46 @@
-import { useEffect, useState } from 'react';
-import './style/LanguageSelectButton.scss'; 
-import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react"
+import "./style/LanguageSelectButton.scss"
+import { useTranslation } from "react-i18next"
 
 function LanguageSelectButton() {
+  const { i18n } = useTranslation()
 
-    const { i18n } = useTranslation()
+  const [enActive, setENActive] = useState(true)
+  const [proActive, setProActive] = useState(true)
 
-    const [enActive, setENActive] = useState(true)
-
-    useEffect(() => {
-      i18n.changeLanguage(enActive ? 'en' : 'de')
-    }, [enActive])
-
+  useEffect(() => {
+    let language = enActive ? "en" : "de"
+    if (proActive) language += "Professional"
+    i18n.changeLanguage(language)
+  }, [enActive, proActive])
 
   return (
-        <div className={`language-button ${enActive ? 'lang-left-active' : 'lang-right-active'}`} onClick={() => {setENActive(!enActive)}}>
-            <span className='text'>EN</span>
-            <span className='text'>DE</span>
-        </div>
-  );
+    <div className="language-button-wrapper">
+      <div
+        className={`language-button ${
+          enActive ? "lang-left-active" : "lang-right-active"
+        }`}
+        onClick={() => {
+          setENActive(!enActive)
+        }}
+      >
+        <span className="text">EN</span>
+        <span className="text">DE</span>
+      </div>
+
+      <div
+        className={`language-button ${
+          proActive ? "lang-left-active" : "lang-right-active"
+        }`}
+        onClick={() => {
+          setProActive(!proActive)
+        }}
+      >
+        <span className="text">PRO</span>
+        <span className="text">CAS</span>
+      </div>
+    </div>
+  )
 }
 
-export default LanguageSelectButton;
+export default LanguageSelectButton
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import translationDE from "./locales/de.json";
-import translationEN from "./locales/en.json";
-import i18next from 'i18next';
-import i18n from 'i18next';
-import { I18nextProvider, initReactI18next } from 'react-i18next';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.scss"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import translationDE from "./locales/de.json"
+import translationEN from "./locales/en.json"
+import translationsENProfessional from "./locales/en-professional.json"
+import translationsDEProfessional from "./locales/de-professional.json"
+import i18next from "i18next"
+import i18n from "i18next"
+import { I18nextProvider, initReactI18next } from "react-i18next"
 
 const resources = {
   en: {
@@ -15,33 +17,36 @@ const resources = {
   },
   de: {
     translation: translationDE,
-  }
-};
+  },
+  enProfessional: {
+    translation: translationsENProfessional,
+  },
+  deProfessional: {
+    translation: translationsDEProfessional,
+  },
+}
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en",
-  fallbackLng: "en",
+  lng: "enProfessional",
+  fallbackLng: "enProfessional",
   interpolation: {
     escapeValue: false,
   },
-});
+})
 
 i18next.init({
   interpolation: { escapeValue: false }, // React already does escaping
-});
+})
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
-
-);
+  <I18nextProvider i18n={i18next}>
+    <App />
+  </I18nextProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
